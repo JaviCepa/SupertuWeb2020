405a3395d42e6044e98b2bcb5e951cb9df167c1c
FaliPixelatto <fali@pixelatto.com>
2020-05-15 12:17:59 +0200

    El menu de navegación se contrae al clickar fuera si está siendo mostrado

--- js/main.js
+++ js/main.js
@@ -176,9 +176,12 @@ $(document).ready(function(){
 	// We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/rpz4991l'
 	(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/rpz4991l';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
 
-	// Recoger el menu de navegación en mobile al navegar
+	// Recoger el menu de navegación en mobile al navegar o clickar fuera
 
-	$("ul.navbar-nav").on("click", function(){
+	$(document).on("click", function(){
+		var navButton = $('button.navbar-toggle');
+
+		if(!navButton.hasClass("collapsed"))
 		$('.navbar-toggle').click();
 	});
 });
