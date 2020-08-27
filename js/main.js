4ac720f0b4b0aaa151d0372d4e330157ce0994eb
FaliPixelatto <fali@pixelatto.com>
2020-08-27 10:53:30 +0200

    Testimonials carousel recovered. Pricing section title fix and style added

--- js/main.js
+++ js/main.js
@@ -8,169 +8,169 @@ $(document).ready(function(){
 	});
 
 
-	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
+	// $("#clients-logo").owlCarousel({
+	// 	autoPlay: 3000,
+	// 	items : 5,
+	// 	itemsDesktop : [1199,5],
+	// 	itemsDesktopSmall : [979,5],
+	// });
 
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
+	// $("#works-logo").owlCarousel({
+	// 	autoPlay: 3000,
+	// 	items : 5,
+	// 	itemsDesktop : [1199,5],
+	// 	itemsDesktopSmall : [979,5],
+	// });
 
-	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
+	// // google map
+	// 	var map;
+	// 	function initMap() {
+	// 	  map = new google.maps.Map(document.getElementById('map'), {
+	// 	    center: {lat: -34.397, lng: 150.644},
+	// 	    zoom: 8
+	// 	  });
+	// 	}
 
 
-	// Counter
+	// // Counter
 
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-		});
+	// $('.counter').counterUp({
+    //     delay: 10,
+    //     time: 1000
+	// 	});
 		
 
-	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Navigation
+	// // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Navigation
 
-	// Global vars
-	var navTarget = $('body').attr('data-page-url');
-	var docTitle = document.title;
-	var docDescription = $("meta[name='description']")[0].content;
-	var History = window.History;
-	var lastUrl = document.URL.substr(0, document.URL.length-1);
-	var newUrl;
+	// // Global vars
+	// var navTarget = $('body').attr('data-page-url');
+	// var docTitle = document.title;
+	// var docDescription = $("meta[name='description']")[0].content;
+	// var History = window.History;
+	// var lastUrl = document.URL.substr(0, document.URL.length-1);
+	// var newUrl;
 
-	// State change event
-	History.Adapter.bind(window,'statechange',function(){
-		var state = History.getState();
+	// // State change event
+	// History.Adapter.bind(window,'statechange',function(){
+	// 	var state = History.getState();
 		
-		newUrl = state.url;
+	// 	newUrl = state.url;
 
-		if(lastUrl != newUrl) {
-			lastUrl = newUrl;
+	// 	if(lastUrl != newUrl) {
+	// 		lastUrl = newUrl;
 
-			// Loading state
-			$('body').addClass('loading');
+	// 		// Loading state
+	// 		$('body').addClass('loading');
 
-			// Load the page
-			$('.page-loader').load( state.hash + ' .page__content', function() {
-
-
-				// Scroll to top
-				$( 'body, html' ).animate({
-					scrollTop: 0
-				}, 0);
-
-				// Find transition time
-				var transitionTime = 0;
-
-				// After current content fades out
-				setTimeout( function() {
-
-					// Remove old content
-					$('.page .page__content').remove();
-
-					// Append new content
-					$('.page-loader .page__content').appendTo('.page');
-
-					// Set page URL
-					$('body').attr('data-page-url', window.location.pathname);
-
-					// Update navTarget
-					navTarget = $('body').attr('data-page-url');
-
-					// Set page title
-					docTitle = $('.page__content').attr('data-page-title');
-					docDescription = $('.page__content').attr('data-page-description');
-					document.title = docTitle;
-					$("meta[name='description']")[0].content = docDescription;
-
-				}, transitionTime);
+	// 		// Load the page
+	// 		$('.page-loader').load( state.hash + ' .page__content', function() {
 
 
-				$("#testimonial-slider").owlCarousel({
-					paginationSpeed : 500,      
-					singleItem:true,
-					autoPlay: 3000,
-				});
+	// 			// Scroll to top
+	// 			$( 'body, html' ).animate({
+	// 				scrollTop: 0
+	// 			}, 0);
+
+	// 			// Find transition time
+	// 			var transitionTime = 0;
+
+	// 			// After current content fades out
+	// 			setTimeout( function() {
+
+	// 				// Remove old content
+	// 				$('.page .page__content').remove();
+
+	// 				// Append new content
+	// 				$('.page-loader .page__content').appendTo('.page');
+
+	// 				// Set page URL
+	// 				$('body').attr('data-page-url', window.location.pathname);
+
+	// 				// Update navTarget
+	// 				navTarget = $('body').attr('data-page-url');
+
+	// 				// Set page title
+	// 				docTitle = $('.page__content').attr('data-page-title');
+	// 				docDescription = $('.page__content').attr('data-page-description');
+	// 				document.title = docTitle;
+	// 				$("meta[name='description']")[0].content = docDescription;
+
+	// 			}, transitionTime);
+
+
+	// 			$("#testimonial-slider").owlCarousel({
+	// 				paginationSpeed : 500,      
+	// 				singleItem:true,
+	// 				autoPlay: 3000,
+	// 			});
 			
 			
-				$("#clients-logo").owlCarousel({
-					autoPlay: 3000,
-					items : 5,
-					itemsDesktop : [1199,5],
-					itemsDesktopSmall : [979,5],
-				});
+	// 			$("#clients-logo").owlCarousel({
+	// 				autoPlay: 3000,
+	// 				items : 5,
+	// 				itemsDesktop : [1199,5],
+	// 				itemsDesktopSmall : [979,5],
+	// 			});
 			
-				$("#works-logo").owlCarousel({
-					autoPlay: 3000,
-					items : 5,
-					itemsDesktop : [1199,5],
-					itemsDesktopSmall : [979,5],
-				});
+	// 			$("#works-logo").owlCarousel({
+	// 				autoPlay: 3000,
+	// 				items : 5,
+	// 				itemsDesktop : [1199,5],
+	// 				itemsDesktopSmall : [979,5],
+	// 			});
 				
-			});
-		}
-	});
+	// 		});
+	// 	}
+	// });
 
 
-	// On clicking a link
+	// // On clicking a link
 
-	if ( $('body').hasClass('ajax-loading') ) {
+	// if ( $('body').hasClass('ajax-loading') ) {
 
-		$(document).on('click', 'a', function (event){
+	// 	$(document).on('click', 'a', function (event){
 
-			// Don't follow link
-			event.preventDefault();
+	// 		// Don't follow link
+	// 		event.preventDefault();
 
-			// Get the link target
-			var thisTarget = $(this).attr('href');
+	// 		// Get the link target
+	// 		var thisTarget = $(this).attr('href');
 
-			// If we don't want to use ajax, or the link is an anchor/mailto/tel
-			if ( $(this).hasClass('js-no-ajax') || /^#/.test(thisTarget) || thisTarget.indexOf("mailto:") >= 0 || thisTarget.indexOf("tel:") >= 0 ) {
+	// 		// If we don't want to use ajax, or the link is an anchor/mailto/tel
+	// 		if ( $(this).hasClass('js-no-ajax') || /^#/.test(thisTarget) || thisTarget.indexOf("mailto:") >= 0 || thisTarget.indexOf("tel:") >= 0 ) {
 
-				// Use the given link
-				window.location = thisTarget;
-			}
+	// 			// Use the given link
+	// 			window.location = thisTarget;
+	// 		}
 
-			// If link is handled by some JS action – e.g. fluidbox
-			else if ( $(this).is('.gallery__item__link') ) {
+	// 		// If link is handled by some JS action – e.g. fluidbox
+	// 		else if ( $(this).is('.gallery__item__link') ) {
 				
-				// Let JS handle it
-			}
+	// 			// Let JS handle it
+	// 		}
 
-			// If link is external
-			else if ( thisTarget.indexOf('http') >= 0 ) {
+	// 		// If link is external
+	// 		else if ( thisTarget.indexOf('http') >= 0 ) {
 
-				// Go to the external link
-				window.open(thisTarget, '_blank');
+	// 			// Go to the external link
+	// 			window.open(thisTarget, '_blank');
 
-			}
+	// 		}
 
-			// If link is internal
-			else {
+	// 		// If link is internal
+	// 		else {
 
-				// Change navTarget
-				navTarget = thisTarget;
+	// 			// Change navTarget
+	// 			navTarget = thisTarget;
 				
-				// Switch the URL via History
-				History.pushState(null, docTitle, thisTarget);
-				History.pushState(null, docDescription, thisTarget);
-			}
+	// 			// Switch the URL via History
+	// 			History.pushState(null, docTitle, thisTarget);
+	// 			History.pushState(null, docDescription, thisTarget);
+	// 		}
 
-		});
+	// 	});
 
-	}
+	// }
 
 	// window.intercomSettings = {
 	// 	app_id: "rpz4991l"
